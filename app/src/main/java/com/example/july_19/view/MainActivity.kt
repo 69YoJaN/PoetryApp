f13369e282631af88f09dce73809ae102e818cf5
69YoJaN <yojan.speroinfosystems@gmail.com>
2024-07-18 23:20:10 -0700

    dependency changes according to older Version done

--- app/src/main/java/com/example/july_19/view/MainActivity.kt
+++ app/src/main/java/com/example/july_19/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.july_19.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.july_19.databinding.ActivityMainBinding
 import com.example.july_19.viewModel.PoetryViewModel
@@ -9,19 +10,22 @@ import com.example.july_19.viewModel.PoetryViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var viewModel : PoetryViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this)[PoetryViewModel::class.java]
+        viewModel = ViewModelProvider(this).get(PoetryViewModel::class.java)
 
-        viewModel.poetry.observe(this) { poetry ->
+
+        viewModel.poetry.observe(this, Observer { poetry ->
             if (poetry.isNotEmpty()) {
                 val getPoetry = poetry[0]
                 binding.title.text = getPoetry.title
             }
-        }
+        })
+
         viewModel.getPoetry()
     }
 }
