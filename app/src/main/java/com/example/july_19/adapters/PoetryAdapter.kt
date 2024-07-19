bcb366d05490538fbfe9fce0b64e2e83ba238ab8
69YoJaN <yojan.speroinfosystems@gmail.com>
2024-07-19 00:06:02 -0700

    added adapter and progressbar

---
+++ app/src/main/java/com/example/july_19/adapters/PoetryAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.july_19.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.july_19.R
+import com.example.july_19.model.Poetry
+
+class PoetryAdapter(private var poetry: List<Poetry>): RecyclerView.Adapter<PoetryAdapter.PoetryViewHolder>() {
+
+    inner class PoetryViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val poetryTitle : TextView = itemView.findViewById(R.id.poetryTitle)
+        val poetryLine : TextView = itemView.findViewById(R.id.poetryLines)
+        val poetryAuthor : TextView = itemView.findViewById(R.id.poetryAuthor)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PoetryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.poetry_list,parent,false)
+        return PoetryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PoetryViewHolder, position: Int) {
+        val poetryList = poetry[position]
+        holder.poetryTitle.text = poetryList.title
+        holder.poetryLine.text = poetryList.lines.toString()
+        holder.poetryAuthor.text = poetryList.author
+    }
+
+    override fun getItemCount(): Int {
+        return poetry.size
+    }
+    fun setPoetry(poetry: List<Poetry>) {
+        this.poetry = poetry
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/july_19/view/MainActivity.kt
+++ app/src/main/java/com/example/july_19/view/MainActivity.kt
@@ -2,30 +2,42 @@ package com.example.july_19.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.july_19.adapters.PoetryAdapter
 import com.example.july_19.databinding.ActivityMainBinding
 import com.example.july_19.viewModel.PoetryViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var viewModel : PoetryViewModel
+    private lateinit var poetryAdapter: PoetryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        viewModel = ViewModelProvider(this).get(PoetryViewModel::class.java)
-
-
-        viewModel.poetry.observe(this, Observer { poetry ->
-            if (poetry.isNotEmpty()) {
-                val getPoetry = poetry[0]
-                binding.title.text = getPoetry.title
-            }
-        })
-
+        setAdapter()
+        viewModel = ViewModelProvider(this)[PoetryViewModel::class.java]
+        getPoetry()
         viewModel.getPoetry()
+
+    }
+
+    private fun setAdapter() {
+        binding.poetryRecyclerView.layoutManager = LinearLayoutManager(this)
+        poetryAdapter = PoetryAdapter(emptyList())
+        binding.poetryRecyclerView.adapter = poetryAdapter
+    }
+
+    private fun getPoetry() {
+        viewModel.poetry.observe(this) { poetry ->
+            if (poetry.isNotEmpty()) {
+                poetryAdapter.setPoetry(poetry)
+            } else {
+                binding.Loader.visibility = View.VISIBLE
+            }
+        }
     }
 }
